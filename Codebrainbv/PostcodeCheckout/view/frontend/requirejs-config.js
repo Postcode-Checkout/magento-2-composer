55ceb76c0534c481d8592798dd175073691f9f35
MBakker_codebrain <m.bakker@codebrain.nl>
2025-09-09 17:05:32 +0200

    - Bit further progress, lot of reading

---
+++ Codebrainbv/PostcodeCheckout/view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_Ui/js/lib/validation/validator': {
+                'Codebrainbv_PostcodeCheckout/js/validation/validator-mixin': true,
+            },
+        },
+    },
+};
