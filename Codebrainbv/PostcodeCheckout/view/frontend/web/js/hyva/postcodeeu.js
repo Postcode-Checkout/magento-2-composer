014f856d93d223c64a219016e0ff4f3d96eebd08
leander <l.maatman@codebrain.nl>
2025-11-25 10:42:09 +0100

    autcomplete js error

--- Codebrainbv/PostcodeCheckout/view/frontend/web/js/hyva/postcodeeu.js
+++ Codebrainbv/PostcodeCheckout/view/frontend/web/js/hyva/postcodeeu.js
@@ -1,6 +1,6 @@
 var fields, elements, validationFields, pcm2_Autocomplete, countryCode; // Declare variables at module level
 
-function pcm2_addLookup(oElement) {
+function pcm2_addLookup(pcm2_Section, oElement) {
 
     // Get country value
     var countryField = oElement;
@@ -30,11 +30,11 @@ function pcm2_addLookup(oElement) {
         // Check if country is supported
         if (pcm2_isSupportedCountry(countryCode)) {
             pcm2_log('PCM2 country is supported, adding postcode lookup');
-            pcm2_hideForm();
+            pcm2_hideForm(pcm2_Section);
         } else {
             pcm2_log('PCM2 country is not supported, no postcode lookup');
             // Show default fields again
-            pcm2_showForm();
+            pcm2_showForm(pcm2_Section);
         }
     });
 
@@ -43,7 +43,7 @@ function pcm2_addLookup(oElement) {
         pcm2_log('PCM2 country is supported, adding postcode lookup');
 
         // Add postcode lookup
-        pcm2_hideForm();
+        pcm2_hideForm(pcm2_Section);
     } else {
         pcm2_log('PCM2 country is not supported, no postcode lookup');
         return;
@@ -51,25 +51,25 @@ function pcm2_addLookup(oElement) {
 
     // Add event listener to pcm2 buttons
     document.addEventListener('click', function (event) {
-        if (event.target && event.target.id === 'pcm2_' + pcm2_section + '_autocomplete_manualbtn') {
+        if (event.target && event.target.id === 'pcm2_' + pcm2_Section + '_autocomplete_manualbtn') {
             pcm2_log('PCM2 manual button clicked, showing default fields');
 
-            pcm2_showForm(true);
+            pcm2_showForm(pcm2_Section, true);
         }
 
-        if (event.target && event.target.id === 'pcm2_' + pcm2_section + '_autocomplete_autobtn') {
+        if (event.target && event.target.id === 'pcm2_' + pcm2_Section + '_autocomplete_autobtn') {
             pcm2_log('PCM2 auto button clicked, showing postcode lookup fields');
 
-            pcm2_hideForm(true);
+            pcm2_hideForm(pcm2_Section, true);
         }
     });
 
     // Initialize lookup functionality
-    pcm2_initLookup();
+    pcm2_initLookup(pcm2_Section);
 
 }
 
-function pcm2_initLookup(pcm2_sSection) {
+function pcm2_initLookup(pcm2_Section) {
 
 
     var iso3Code = pcm2_convertIso2ToIso3(countryCode);
@@ -77,12 +77,7 @@ function pcm2_initLookup(pcm2_sSection) {
     pcm2_log('Converted country code to ISO3:', iso3Code);
 
     // Initialize autocomplete
-    const searchField = document.getElementById('pcm2_' + pcm2_sSection + '_autocomplete_search');
-
-    if (!AutocompleteAddress) {
-        pcm2_log('PCM2 AutocompleteAddress not available');
-        return;
-    }
+    const searchField = document.getElementById('pcm2_' + pcm2_Section + '_autocomplete_search');
 
     pcm2_Autocomplete = new AutocompleteAddress(searchField, {
         autocompleteUrl: config.pcm2_config.api_urls.international_suggest,
@@ -139,10 +134,10 @@ function pcm2_initLookup(pcm2_sSection) {
     });
 }
 
-function pcm2_clearAllAddressFields() {
+function pcm2_clearAllAddressFields(pcm2_Section) {
     // haal velden en validatie-elementen op
-    fields = pcm2_getFields();
-    validationFields = pcm2_getValidationFields();
+    fields = pcm2_getFields(pcm2_Section);
+    validationFields = pcm2_getValidationFields(pcm2_Section);
 
     // kleine helper om events te versturen
     const fireUpdates = (el) => {
@@ -152,7 +147,7 @@ function pcm2_clearAllAddressFields() {
     };
 
     // zoekveld (autocomplete) leegmaken
-    const searchField = document.getElementById('pcm2_' + pcm2_section + '_autocomplete_search');
+    const searchField = document.getElementById('pcm2_' + pcm2_Section + '_autocomplete_search');
     if (searchField) {
         searchField.value = '';
         fireUpdates(searchField);
@@ -163,7 +158,7 @@ function pcm2_clearAllAddressFields() {
         validationFields.resultWrapper.innerHTML = '';
     } else {
         // fallback op id, voor het geval validationFields nog niet klaar staat
-        const resultWrapper = document.getElementById('pcm2_' + pcm2_section + '_autocomplete_result_wrapper');
+        const resultWrapper = document.getElementById('pcm2_' + pcm2_Section + '_autocomplete_result_wrapper');
         if (resultWrapper) resultWrapper.innerHTML = '';
     }
 
@@ -221,10 +216,10 @@ function pcm2_fillAddressFields(result) {
     }
 }
 
-function pcm2_updatePreview(errorMsg = false) {
+function pcm2_updatePreview(pcm2_Section, errorMsg = false) {
 
-    fields = pcm2_getFields();
-    validationFields = pcm2_getValidationFields();
+    fields = pcm2_getFields(pcm2_Section);
+    validationFields = pcm2_getValidationFields(pcm2_Section);
 
     if (errorMsg) {
         validationFields.resultWrapper.innerHTML = '<p style="color:red;">Adres kon niet worden gevonden, controleer Postcode en Huisnummer of voer handmatig in.</p>';
@@ -248,11 +243,11 @@ function pcm2_updatePreview(errorMsg = false) {
     validationFields.resultWrapper.innerHTML = html;
 }
 
-function pcm2_hideForm(pcm2_section, defaultForm = false) {
+function pcm2_hideForm(pcm2_Section, defaultForm = false) {
     // Add param to find out if its the shipping or billing form
-    fields = pcm2_getFields();
-    elements = pcm2_getElements();
-    validationFields = pcm2_getValidationFields();
+    fields = pcm2_getFields(pcm2_Section);
+    elements = pcm2_getElements(pcm2_Section);
+    validationFields = pcm2_getValidationFields(pcm2_Section);
 
     // If checkbox, hide our fields and show default fields
     if (defaultForm) {
@@ -272,15 +267,15 @@ function pcm2_hideForm(pcm2_section, defaultForm = false) {
     } else {
         // Hide address fields
         var html =
-            '<div class="field" id="pcm2_' + pcm2_section + 'autocomplete_search_wrapper">' +
-            '  <label class="label" for="pcm2_autocomplete_search"><span> ' + translate.pcm2_translations.search_address + ' </span></label>' +
-            '  <div class="control"><input id="pcm2_' + pcm2_section + 'autocomplete_search" name="pcm2_autocomplete_search" type="text" class="input-text" required /></div>' +
+            '<div class="field" id="pcm2_' + pcm2_Section + '_autocomplete_search_wrapper">' +
+            '  <label class="label" for="pcm2_' + pcm2_Section + '_autocomplete_search"><span> ' + translate.pcm2_translations.search_address + ' </span></label>' +
+            '  <div class="control"><input id="pcm2_' + pcm2_Section + '_autocomplete_search" name="pcm2_autocomplete_search" type="text" class="input-text" required /></div>' +
             '</div>' +
-            '<div class="field" id="pcm2_' + pcm2_section + 'autocomplete_result_wrapper">' +
+            '<div class="field" id="pcm2_' + pcm2_Section + '_autocomplete_result_wrapper">' +
             '</div>' +
             '<div class="field"><div class="control">' +
-            '  <button type="button" class="action secondary" id="pcm2_' + pcm2_section + 'autocomplete_manualbtn">' + translate.pcm2_translations.manual + '</button> ' +
-            '  <button type="button" class="action secondary" id="pcm2_' + pcm2_section + 'autocomplete_autobtn" style="display:none;">' + translate.pcm2_translations.automatic + '</button>' +
+            '  <button type="button" class="action secondary" id="pcm2_' + pcm2_Section + '_autocomplete_manualbtn">' + translate.pcm2_translations.manual + '</button> ' +
+            '  <button type="button" class="action secondary" id="pcm2_' + pcm2_Section + '_autocomplete_autobtn" style="display:none;">' + translate.pcm2_translations.automatic + '</button>' +
             '</div></div>';
 
         // Check if it doesnt already exists
@@ -308,12 +303,12 @@ function pcm2_hideForm(pcm2_section, defaultForm = false) {
         }
     }
 
-    pcm2_initLookup();
+    pcm2_initLookup(pcm2_Section);
 }
 
 function pcm2_showForm(defaultForm = false) {
-    fields = pcm2_getFields();
-    elements = pcm2_getElements();
+    fields = pcm2_getFields(pcm2_Section);
+    elements = pcm2_getElements(pcm2_Section);
 
     var domKeys = Object.keys(fields);
 
@@ -356,40 +351,41 @@ function pcm2_showForm(defaultForm = false) {
 
 }
 
-function pcm2_getFields(pcm2_sSection) {
+function pcm2_getFields(pcm2_Section) {
 
     fields = {
-        address_1: document.getElementById(pcm2_sSection + '-street-0'),
-        address_2: document.getElementById(pcm2_sSection + '-street-1'),
-        address_3: document.getElementById(pcm2_sSection + '-street-2'),
-        postcode: document.getElementById(pcm2_sSection + '-postcode'),
-        city: document.getElementById(pcm2_sSection + '-city'),
-        region: document.getElementById(pcm2_sSection + '-region'),
+        address_1: document.getElementById(pcm2_Section + '-street-0'),
+        address_2: document.getElementById(pcm2_Section + '-street-1'),
+        address_3: document.getElementById(pcm2_Section + '-street-2'),
+        postcode: document.getElementById(pcm2_Section + '-postcode'),
+        city: document.getElementById(pcm2_Section + '-city'),
+        region: document.getElementById(pcm2_Section + '-region'),
     };
 
     return fields;
 }
 
-function pcm2_getElements(pcm2_sSection) {
+function pcm2_getElements(pcm2_Section) {
+     console.log(pcm2_Section);
 
     elements = {
         // Since street 0,1,2 have a single parent fieldset, we need to get the parent element once
-        addresses: document.getElementById(pcm2_sSection + '-street-0').closest('div.group-street'),
-        postcode: document.getElementById(pcm2_sSection + '-postcode').closest('div.field'),
-        city: document.getElementById(pcm2_sSection + '-city').closest('div.field'),
-        region: document.getElementById(pcm2_sSection + '-region').closest('div.field'),
-        country: document.getElementById(pcm2_sSection + '-country_id').closest('div.field'),
+        address_1: document.getElementById(pcm2_Section + '-street-0').closest('div.group-street'),
+        postcode: document.getElementById(pcm2_Section + '-postcode').closest('div.field'),
+        city: document.getElementById(pcm2_Section + '-city').closest('div.field'),
+        region: document.getElementById(pcm2_Section + '-region').closest('div.field'),
+        country: document.getElementById(pcm2_Section + '-country_id').closest('div.field'),
     };
 
     return elements;
 }
 
-function pcm2_getValidationFields() {
+function pcm2_getValidationFields(pcm2_Section) {
     validationFields = {
-        searchWrapper: document.getElementById('pcm2_autocomplete_search_wrapper'),
-        resultWrapper: document.getElementById('pcm2_autocomplete_result_wrapper'),
-        manualBtn: document.getElementById('pcm2_autocomplete_manualbtn'),
-        autoBtn: document.getElementById('pcm2_autocomplete_autobtn')
+        searchWrapper: document.getElementById('pcm2_' + pcm2_Section + '_autocomplete_search_wrapper'),
+        resultWrapper: document.getElementById('pcm2_' + pcm2_Section + '_autocomplete_result_wrapper'),
+        manualBtn: document.getElementById('pcm2_' + pcm2_Section + '_autocomplete_manualbtn'),
+        autoBtn: document.getElementById('pcm2_' + pcm2_Section + '_autocomplete_autobtn')
     };
 
     return validationFields;
@@ -426,27 +422,26 @@ function pcm2_log(msg, data) {
 
         pcm2_log(config.pcm2_config);
 
-        let pcm2_sSection = '';
+        let pcm2_Section = '';
         let oElement = '';
 
-        pcm2_sSection = 'shipping';
-        console.log(pcm2_sSection);
-        oElement = document.getElementById(pcm2_sSection + '-country_id');
+        pcm2_Section = 'shipping';
+        oElement = document.getElementById(pcm2_Section + '-country_id');
 
         if (oElement) {
-            pcm2_addLookup(oElement);
+            pcm2_addLookup(pcm2_Section, oElement);
             oElement.addEventListener('change', function () {
-                pcm2_addLookup(oElement);
+                pcm2_addLookup(pcm2_Section, oElement);
             });
         }
 
-        pcm2_sSection = 'billing';
-        oElement = document.getElementById(pcm2_sSection + '-country_id');
+        pcm2_Section = 'billing';
+        oElement = document.getElementById(pcm2_Section + '-country_id');
 
         if (oElement) {
-            pcm2_addLookup(oElement);
+            pcm2_addLookup(pcm2_Section, oElement);
             oElement.addEventListener('change', function () {
-                pcm2_addLookup(oElement);
+                pcm2_addLookup(pcm2_Section, oElement);
             });
         }
     }
